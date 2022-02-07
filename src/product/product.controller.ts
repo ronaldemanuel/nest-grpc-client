69c1684d625e482e6b3d921f393cf746f8e09e29
ronaldemanuel <emronald15el@gmail.com>
2022-02-07 15:35:23 -0300

    feat: implement crud methods on product controller

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,10 +1,13 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   Inject,
+  NotFoundException,
   OnModuleInit,
   Param,
+  Patch,
   Post,
 } from '@nestjs/common';
 import { ClientGrpc } from '@nestjs/microservices';
@@ -12,12 +15,13 @@ import { lastValueFrom, Observable } from 'rxjs';
 import { CreateProduct } from './interfaces/create-product.interface';
 import { ProductById } from './interfaces/product-by-id.interface';
 import { Product } from './interfaces/product.interface';
+import { UpdateProduct } from './interfaces/update-product.interface';
 
 interface ProductService {
   create(data: CreateProduct): Observable<Product>;
-  // update;
+  update(data: UpdateProduct): Observable<Product>;
   findOne(data: ProductById): Observable<Product>;
-  findAll(): Observable<any>;
+  findAll(): Observable<{ data: Product[] }>;
   delete(data: ProductById): Observable<any>;
 }
 
@@ -32,4 +36,34 @@ export class ProductController implements OnModuleInit {
       this.client.getService<ProductService>('ProductService');
   }
 
+  @Get()
+  findALl() {
+    const res = this.productGrpcService.findAll();
+    console.log(lastValueFrom(res));
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: number) {
+    try {
+      return await lastValueFrom(this.productGrpcService.findOne({ id }));
+    } catch (error) {
+      throw new NotFoundException('product not found');
+    }
+  }
+
+  @Post()
+  create(@Body() data: CreateProduct) {
+    return lastValueFrom(this.productGrpcService.create(data));
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: number, @Body() data: UpdateProduct) {
+    const req = { id, ...data };
+    return lastValueFrom(this.productGrpcService.update(req));
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: number) {
+    return lastValueFrom(this.productGrpcService.delete({ id }));
+  }
 }
